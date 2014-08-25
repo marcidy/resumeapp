b2acea2efd283318e081c0233a2cdeb842b09091
Matthew Arcidy <marcidy@gmail.com>
2014-08-25 01:25:39 -0400

    lock/unlock is working for editing elements

--- resumes/scripts/resumes.js
+++ resumes/scripts/resumes.js
@@ -56,10 +56,40 @@ Subject.prototype.notify = function( context ){
     }
 };
 
+Subject.prototype.lock = function( who ){
+    this.observers.removeAt( this.observers.indexOf( who, 0) );
+    var observerCount = this.observers.count();
+    for (var i=0; i < observerCount; i++){
+        this.observers.get(i).lock();
+    }
+    this.observers.add( who );
+};
+
+
 function Observer() {
-    this.update = function () {
+    this.lock = function() {
     };
 }
+
+function unLockSubject(elem, subj) {
+    elem.unlock = function() {
+        $(elem).unbind('click');
+        $(elem).click(function() {
+            subj.unlock();
+        });
+    };
+}
+
+
+function lockObject(obj, subj) {
+    obj.lock = function() {
+        $(obj).unbind('click');
+        $(obj).click( function() {
+            subj.unlock();
+        });
+    };
+}
+        
 
 function extend( extension, obj ){
     for ( var key in extension ){
@@ -130,47 +160,58 @@ var resumeIndex = {
     'cvitemizeditem' : cvItemizedItems
 };
 
-var getForm  = function(elem, callback){
+var rGet = function(elem, attrib) {
+    obj = resumeIndex[resumeElementType(elem)];
+    if (attrib in obj) {
+        return obj[attrib];
+    }
+};
+
+var getForm  = function(elem){
     var form_getter = resumeIndex[resumeElementType(elem)].form_getter;
     var form = document.createElement("div");
+    // Right, I know that space looks wrong, but it's not.
+    $(form).load("/forms/resume_edit " + form_getter, populateForm(elem));
     $(form).attr("id", "form_" + $(elem).attr("id"));
-    $(form).load("/forms/resume_edit " + form_getter, callback);
     // FIXME:
     // callback fires before form_getter selector
     // Add form_getter to populate form as well
     return form;
 };
 
-var populateForm = function(selectors, accessors, data){
+var populateForm = function(elem){
     return function(resp, status, xhr) {
+        var form_getter = rGet(elem, 'form_getter');
+        var selectors = rGet(elem, 'form_prefil');
+        var data = ['nothing'];
         for (var i=0; i < selectors.length; i++) {
             // get form.getter
             // use it on resp because callback fires
             // before form_getter in getForm is applied
-            var obj = $(resp).find(selectors[i]);
-            $(resp).find(selectors[i]).val(data[i]);
-            var out = obj.val();
-            obj.innerText = "cock sucker";
-            var out2 = obj.val();
+            var test = $(resp).find(form_getter)[0];
+            var obj = $(test).find(selectors[i])[0];
+            var stuff = $(elem).val();
+            $(obj).val($(elem).val());
         }
     };
 };
 
 var editMenus = { 
     'trigger' : {'method' : 'click'},
-    'actions' : function(elem) {
+    'actions' : function(elem, subj) {
         return function() {
             $(elem).hide();
-            selectors = resumeIndex[resumeElementType(elem)].form_prefil;
-            accessors = ['html'];
-            data = ["testicles"];
-            form = $("[id=form_"+$(elem).attr("id")+"]");
+            subj.lock(elem);
+            var selectors = resumeIndex[resumeElementType(elem)].form_prefil;
+            var accessors = ['html'];
+            var data = ["testicles"];
+            var form = $("[id=form_"+$(elem).attr("id")+"]");
             if (form.length === 0) {
-                form = getForm(elem, populateForm(selectors, accessors, data));
+                form = getForm(elem);
                 $(elem).after(form);
             }
             $(form).show();
-            $(form)[editMenus.end.method](editMenus.last(elem));
+            // $(form)[editMenus.end.method](editMenus.last(elem));
         };
     },
     'end' : {'method' : 'mouseout'},
@@ -178,42 +219,55 @@ var editMenus = {
         return function() {
             $(elem).css("color", "purple");
             $(elem).show();
-            form = $("[id=form_"+$(elem).attr("id")+"]");
+            var form = $("[id=form_"+$(elem).attr("id")+"]");
             if (form.length > 0) {
                 form.hide();
             }
         };
     },
-    'init' : function(elem) {
-        $(elem)[editMenus.trigger.method](editMenus.actions(elem));
+    'init' : function(elem, subj) {
+        $(elem).unbind('click');
+        $(elem)[editMenus.trigger.method](editMenus.actions(elem, subj));
     }
 };
 
 
 $(document).ready(function(){
+    var sections = $("[id^=section]");
+    var cvitemizeditems = $("[id^=cvitemized_item]");
     var x = editMenus;
-    var sections = $("[id^=section_]");
-    var cventries = $("[id^=cventry_]");
-    var cvlistitems = $("[id^=cvlistitem_]");
-    var cvcolumns = $("[id^=cvcolumn_]");
-    var cvcolumitems = $("[id^=cvcolumn_item_]");
-    var cvitemheadings = $("[id^=cvitemized_heading_]");
-    var cvitemizeditems = $("[id^=cvitemized_item_]");
-    var cvcolumngroup = $("[id^=cvcolumngroup_]");
-
-    elem = $("[id=cvitemized_item_5]");
-    form = getForm(elem);
-    $(elem).after(form);
-    var g = $(form).find('textarea');
-    $(form).find('textarea').defaultValue = "test";
+    var elem = $("[id=cvitemized_item_5]")[0];
+    var g = document.createElement("div");
+    $(g).append(document.createElement("textarea"));
+    var ta = $(g).find('textarea')[0];
+    ta.cols = "80";
+    ta.rows = "1";
+    $(g).find('textarea')[0].defaultValue = elem.innerText;
+    $(elem).after(g);
 
 
     for (var i=0; i < sections.length; i++) {
         //x.init(sections[i]);
     }
 
+    var subj = new Subject();
+
+    subj.unlock = function(){
+        var observerCount = this.observers.count();
+        for (var i=0; i < observerCount; i++){
+            obs = this.observers.get(i);
+            $(obs).unbind('click');
+            x.init(obs, subj);
+        }
+    };
+
     for (i=0; i < cvitemizeditems.length; i++){
-          x.init(cvitemizeditems[i]);
+        item = cvitemizeditems[i];
+        x.init(item, subj);
+        extend(new Observer(), item);
+        unLockSubject(item, subj);
+        lockObject(item, subj);
+        subj.addObserver(item);
     }
 }
 );
